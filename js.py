008c802faa89da7c73d34f91bb761f4dcb2ad465
sv3a <sveas93@gmail.com>
2019-11-20 10:45:17 +0100

    Export job adds to json file

--- js.py
+++ js.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 import sys
+import json
 from bs4 import BeautifulSoup
 """
 Selenium imports
@@ -23,8 +24,19 @@ class JobAdd():
     add_heading = ""
     add_content = ""
 
+    """
+    Distill object into a dictionary and return it
+    """
+    def get_dict(self):
+        dict = {
+                "add_url"    : self.add_url,
+                "company_url": self.company_url,
+                "company"    : self.company,
+                "add_heading": self.add_heading,
+                "add_content": self.add_content
+                }
+        return dict
 
-jobs = []
 
 url = "https://www.jobindex.dk/jobsoegning/ingenioer/maskiningenioer/" + \
       "storkoebenhavn"
@@ -57,22 +69,25 @@ if driver.find_element_by_class_name('show-on-load'):
 adds = driver.find_elements_by_xpath("//div[@class='PaidJob']")
 
 for add in adds:
+    # Create job object
     job = JobAdd()
+
     html = add.get_attribute('innerHTML')
     soup = BeautifulSoup(html, 'html.parser')
 
+    # Extract add details
     links = soup.find_all("a")
-
     job.company_url = links[0].get('href')
-    job.add_url = links[1].get('href')
-
+    job.add_url     = links[1].get('href')
+    job.company     = links[2].string
     job.add_heading = links[1].string
-    job.company = links[2].string
 
+    # Extract job add content
     content = ""
     for text in soup.find_all('p'):
         content = content + text.get_text().strip() + '\n'
-
     job.add_content = content
 
-    jobs.append(job)
+    # Write to file
+    with open("data.json", "a") as write_file:
+        json.dump(job.get_dict(), write_file, indent=4, separators=(',', ': '))
