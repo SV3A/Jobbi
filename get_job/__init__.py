6bba24c2862ab8531a050e5417b763601cff0438
sv3a <sveas93@gmail.com>
2019-11-21 21:44:45 +0100

    Replaced Selenium approach

--- get_job/__init__.py
+++ get_job/__init__.py
@@ -1,20 +1,10 @@
 #!/bin/python3
 import sys
 import json
+from requests import get
 from bs4 import BeautifulSoup
-"""
-Selenium imports
-webdriver       : used for launcing the browser
-By              : search using the By parameter
-WebDriverWait   : wait for page load
-EC              : specify what to look for
-TimeoutException: handle timeout
-"""
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from requests.exceptions import RequestException
+from contextlib import closing
 
 
 class JobAdd():
@@ -41,42 +31,21 @@ class JobAdd():
 url = "https://www.jobindex.dk/jobsoegning/ingenioer/maskiningenioer/" + \
       "storkoebenhavn"
 
-# Setup webdriver
-option = webdriver.ChromeOptions()
-option.add_argument(" — incognito")
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',
-                          chrome_options=option)
-
-# Open page
-driver.get(url)
-
-# Wait for page to load
-timeout = 10
 try:
-    WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(
-        (By.XPATH, "//div[@class='PaidJob']")))
-except TimeoutException:
-    print("Timed out waiting for page to load")
-    driver.quit()
+    with closing(get(url, stream=True)) as resp:
+        resp.content
+except RequestException as e:
+    sys.exit('Error during requests to {0} : {1}'.format(url, str(e)))
 
-print("Finished loading")
-
-# Check if modal appears and close it
-if driver.find_element_by_class_name('show-on-load'):
-    driver.find_element_by_xpath("//div[contains(@class,'show-on-load')]" +
-                                 "//button[@class='close']").click()
-
-adds = driver.find_elements_by_xpath("//div[@class='PaidJob']")
+html = BeautifulSoup(resp.content, 'html.parser')
+adds = html.find_all("div", class_="PaidJob")
 
 for add in adds:
     # Create job object
     job = JobAdd()
 
-    html = add.get_attribute('innerHTML')
-    soup = BeautifulSoup(html, 'html.parser')
-
     # Extract add details
-    links = soup.find_all("a")
+    links = add.find_all("a")
     job.company_url = links[0].get('href')
     job.add_url     = links[1].get('href')
     job.company     = links[2].string
@@ -84,10 +53,11 @@ for add in adds:
 
     # Extract job add content
     content = ""
-    for text in soup.find_all('p'):
+    for text in add.find_all('p'):
         content = content + text.get_text().strip() + '\n'
     job.add_content = content
 
     # Write to file
-    with open("data.json", "a") as write_file:
-        json.dump(job.get_dict(), write_file, indent=4, separators=(',', ': '))
+    with open("data.json", "a", encoding="utf8") as write_file:
+        json.dump(job.get_dict(), write_file, indent=4, separators=(',', ': '),
+                  ensure_ascii=False)
