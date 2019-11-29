b934e15a9a4da9375be51b7e584f44904a4e4082
sv3a <sveas93@gmail.com>
2019-11-29 23:36:38 +0100

    Added scraper

--- get_job/scraper.py
+++ get_job/scraper.py
@@ -58,8 +58,44 @@ class Jobindex(_Scraper):
 
 
 class Jobfinder(_Scraper):
-    # TODO
-    pass
+    def __init__(self, url):
+        self.url = url
+
+        # Instantiate Soup object
+        self._get_html()
+
+        # Populate list containing the inner html of the divs with PaidJob
+        # class
+        self.adds_html = self.html_obj.find_all("div", class_="user-jobs__" +
+                                                "content-item")
+
+    def parse_adds(self):
+        for add in self.adds_html:
+            # Create job object
+            job = jobs.JobAdd("jobfinder")
+
+            # Extract add details
+            title = add.find("h2")
+            company = add.find("div", class_=
+                               "node-list__item-recruiter")
+            workplace = add.find("div", class_=
+                                 "node-list__item-location").contents[1]
+            job.company_url = "https://www.jobfinder.dk"+company.a.get("href")
+            job.add_url     = "https://www.jobfinder.dk"+title.a.get("href")
+            job.company     = company.a.string
+            job.add_heading = title.a.string
+
+            # Extract job add content
+            content = "Lokation: " + workplace + "\n"
+            content = content+add.find("div", class_=
+                                       "node-list__item-description").p.string
+            job.add_content = content
+
+            # Calculate hash
+            job.hash()
+
+            # Append to JobAdd objects list
+            self.adds.append(job)
 
 
 def download_src(url):
